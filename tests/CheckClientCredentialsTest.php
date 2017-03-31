20f9214327986f8326ce9c138d08c8ecce485cb6
Hamid Alaei <hamid.a85@gmail.com>
2017-03-31 19:48:46 +0430

    MissingScopeException instead of AuthenticationException

--- tests/CheckClientCredentialsTest.php
+++ tests/CheckClientCredentialsTest.php
@@ -50,7 +50,7 @@ class CheckClientCredentialsTest extends TestCase
     }
 
     /**
-     * @expectedException Illuminate\Auth\AuthenticationException
+     * @expectedException Laravel\Passport\Exceptions\MissingScopeException
      */
     public function test_exception_is_thrown_if_token_does_not_have_required_scopes()
     {
